a94fc352e57a300906b419aec4b3aecdfde9ccf0
Jhp <360891090@qq.com>
2019-05-20 19:44:24 +0800

    1

--- js-t2/simpleGame.js
+++ js-t2/simpleGame.js
@@ -9,24 +9,29 @@ function a() {
     if (num >= 4 && num < 6) {
         killer = 1;
         console.log(killer);
+        common = num - killer;
     }
     else if (num >= 6 && num < 9) {
         killer = 2;
+        common = num - killer;
     }
     else if (num >= 9 && num < 12) {
         killer = 3;
+        common = num - killer;
     }
     else if (num >= 12 && num < 16) {
         killer = 4;
+        common = num - killer;
     }
     else if (num >= 16 && num <= 18) {
         killer = 5;
+        common = num - killer;
     }
     else {
         killer = null;
         num = null;
+        common = null;
     }
-    common = num - killer;
 
     document.getElementById('killerNum').innerHTML = killer;
     document.getElementById('commonNum').innerHTML = common;
