853a4b042991379845e39fa3679d26cfae023ba8
Jhp <360891090@qq.com>
2019-05-24 18:43:53 +0800

    1

--- js-t2/simpleGame.js
+++ js-t2/simpleGame.js
@@ -1,39 +1,71 @@
 function back() {
     window.location.href='main.html';
 }
-
+var killer;
+var common;
+var dragNum = document.getElementById('dragBar');
+var num = document.getElementById('inputNumber');
+function change() {
+        num.value = dragNum.value;
+}
 function a() {
-    var num = Number(am.member.value);
-    var killer;
-    var common;
-    if (num >= 4 && num < 6) {
-        killer = 1;
-        console.log(killer);
-        common = num - killer;
-    }
-    else if (num >= 6 && num < 9) {
-        killer = 2;
-        common = num - killer;
-    }
-    else if (num >= 9 && num < 12) {
-        killer = 3;
-        common = num - killer;
-    }
-    else if (num >= 12 && num < 16) {
-        killer = 4;
-        common = num - killer;
-    }
-    else if (num >= 16 && num <= 18) {
-        killer = 5;
-        common = num - killer;
+    if (num.value >= 4 && num.value <= 18){
+        dragNum.value = num.value;
+        if (num.value >= 4 && num.value < 6) {
+            killer = 1;
+        }
+        else if (num.value >= 6 && num.value < 9) {
+            killer = 2;
+        }
+        else if (num.value >= 9 && num.value < 12) {
+            killer = 3;
+        }
+        else if (num.value >= 12 && num.value < 16) {
+            killer = 4;
+        }
+        else {
+            killer = 5;
+        }
+        //计算出平民身份人数
+        common = Number(num.value) - killer;
+        //分配身份
+        //循环一个1-n序号数组
+        var players = [];
+        for (var i = 1; i <= num.value;i++){
+            players.push(i);
+        }
+        var killerNum = [];
+        killerNum.length = killer;
+        killerNum.fill = "杀手";
+        var commonNum = [];
+        commonNum.length = common;
+        commonNum.fill = "平民";
+        var status = killerNum.concat(commonNum);
+        function shuffle(status) {
+            var i , j , temp;
+            for (i = status.length - 1; i > 0; i--){
+                j = Math.floor(Math.random() * (i + 1));
+                temp = status[i];
+                status[i] = status[j];
+                status[j] = temp;
+            }
+            return status;
+        }
     }
     else {
         killer = null;
         num = null;
         common = null;
     }
-
+    //身份人数显示
     document.getElementById('killerNum').innerHTML = killer;
     document.getElementById('commonNum').innerHTML = common;
+    document.getElementById('numNow').innerHTML = num.value;
 }
+
+
+
+
+
+
 
