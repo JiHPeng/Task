9232d049ed9a87baa5586b470ff05b7eb0e39090
Jhp <360891090@qq.com>
2019-05-25 11:03:51 +0800

    1

--- js-t2/simpleGame.js
+++ js-t2/simpleGame.js
@@ -6,7 +6,7 @@ var common;
 var dragNum = document.getElementById('dragBar');
 var num = document.getElementById('inputNumber');
 function change() {
-        num.value = dragNum.value;
+    num.value = dragNum.value;
 }
 function a() {
     if (num.value >= 4 && num.value <= 18){
@@ -29,40 +29,43 @@ function a() {
         //计算出平民身份人数
         common = Number(num.value) - killer;
         //分配身份
-        //循环一个1-n序号数组
-        var players = [];
-        for (var i = 1; i <= num.value;i++){
-            players.push(i);
-        }
+        //将杀手和平民按数量组成数组
         var killerNum = [];
         killerNum.length = killer;
-        killerNum.fill = "杀手";
+        console.log(killer);
+        console.log(killerNum.length);
+        killerNum.fill("杀手");
         var commonNum = [];
         commonNum.length = common;
-        commonNum.fill = "平民";
+        commonNum.fill("平民");
         var status = killerNum.concat(commonNum);
-        function shuffle(status) {
-            var i , j , temp;
-            for (i = status.length - 1; i > 0; i--){
-                j = Math.floor(Math.random() * (i + 1));
-                temp = status[i];
-                status[i] = status[j];
-                status[j] = temp;
-            }
-            return status;
+        //声明一个新数组，用来存放打乱顺序后的身份
+        var playerNum = [];
+        //循环，每次都会在新数组中的随机位置生
+        // 成一个身份，同时，原数组的身份被删除
+        for (;status.length > 0;){
+            var randomNum = Math.floor(Math.random()*status.length);
+            playerNum.push(status[randomNum]);
+            status.splice(randomNum , 1);
         }
+        console.log(status);
+        console.log(playerNum);
     }
     else {
-        killer = null;
-        num = null;
-        common = null;
+        alert('人数必须在4-18之间');
+        killer = '';
+        num.value = '';
+        common = '';
     }
     //身份人数显示
+    console.log(num.value);
+    console.log(killer);
+    console.log(common);
+}
+function set() {
     document.getElementById('killerNum').innerHTML = killer;
     document.getElementById('commonNum').innerHTML = common;
-    document.getElementById('numNow').innerHTML = num.value;
 }
-
 
 
 
