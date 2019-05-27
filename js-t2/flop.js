e27194e0c75fabf07b3603ab56fd0c9358ba105d
Jhp <360891090@qq.com>
2019-05-27 21:42:36 +0800

    1

--- js-t2/flop.js
+++ js-t2/flop.js
@@ -18,7 +18,10 @@ function check() {
     //设定分配的上限为playerNum
     if (i <= playerNum.length - 1) {
         //初始时，i=0，为整数，点击后显示身份
+        //变量i每次点击增加0.5，当i为整数时，也就是初始页面时
+        //点击后产生的效果
         if (Number.isInteger(i) === true){
+            //通过匹配字段来操作dom元素内容
             if (playerNum[i].match('杀手')){
                 img = '<img src="../images/js2/killer.png">';
                 identity = '<p>角色:杀手</p>';
@@ -27,17 +30,20 @@ function check() {
                 img = '<img src="../images/js2/common.png">';
                 identity = '<p>角色:平民</p>';
             }
-            $(".main-box-inner-top").append(img,identity);   //显示玩家的身份
+            //显示玩家的身份
+            $(".main-box-inner-top").append(img,identity);
             //显示当前玩家的序号
             $(".main-box-title").text(""+o+"");
+            //显示下一位玩家的序号
+            //如果达到了参与玩家的上限，则显示传递给法官
             if (i < playerNum.length - 1){
-                //显示下一位玩家的序号
                 $("#check").val("隐藏并传递给"+p+"号");
             }
             else {
                 $("#check").val("隐藏并传递给法官")
             }
         }
+        //i为非整数时显示的内容
         else {
             $(".main-box-title").text(""+p+"");
             $("#check").val("查看"+p+"号玩家身份")
@@ -45,6 +51,7 @@ function check() {
         i = i + 0.5;
         console.log(i);
     }
+    //最后一名玩家隐藏身份后
     else {
         $("#check").val("法官查看身份")
     }
